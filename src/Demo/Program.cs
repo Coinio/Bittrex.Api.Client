8c6b43bd8bcaa93a28dc776afe35734cf11e8425
Ramon Smits <ramon.smits@gmail.com>
2017-12-02 21:35:31 +0100

    Moved source to src folder and added demo project.

---
+++ src/Demo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+using Bittrex.Api.Client;
+
+class Program
+{
+    static async Task Main()
+    {
+        var client = new BittrexClient("key", "secret");
+
+        var result = await client.GetMarkets()
+            .ConfigureAwait(false);
+
+        foreach (var market in result.Result)
+        {
+            await Console.Out.WriteLineAsync(market.MarketName)
+                .ConfigureAwait(false);
+        }
+    }
+}
