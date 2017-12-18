27e62eb0f5f91f72f1cb2d31e3898b77fb8bfeb6
scotcowley <scotc@identitag.net>
2017-12-18 16:18:47 +0200

    Added support for deposit and withdrawal history

---
+++ src/Bittrex.Api.Client/Models/WithdrawalHistory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Bittrex.Api.Client.Models
+{
+    /// <summary>
+    /// An open order result from the /market/getopenorders end point
+    /// </summary>
+    public class WithdrawalHistory
+    {
+        public int Id { get; set; }
+        public Decimal Amount { get; set; }
+        public String Currency { get; set; }
+        public int Confirmations { get; set; }
+        public DateTime LastUpdated { get; set; }
+        public String TxId { get; set; }
+        public String CryptoAddress { get; set; }
+    }
+}
